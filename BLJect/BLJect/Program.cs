247c65956020cddc0f6a1ce473314b587f00d475
Auios <LoneAuios@gmail.com>
2020-12-16 00:43:01 -0500

    Initial Commit

---
+++ BLJect/BLJect/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BLJect
+{
+    class Program
+    {
+        // Source: https://stackoverflow.com/a/5519517/5761668
+        static void ExecuteCommand(string command)
+        {
+            var processInfo = new ProcessStartInfo("cmd.exe", "/c " + command);
+            processInfo.CreateNoWindow = true;
+            processInfo.UseShellExecute = false;
+            processInfo.RedirectStandardError = true;
+            processInfo.RedirectStandardOutput = true;
+
+            var process = Process.Start(processInfo);
+
+            process.OutputDataReceived += (object sender, DataReceivedEventArgs e) =>
+                Console.WriteLine("output>>" + e.Data);
+            process.BeginOutputReadLine();
+
+            process.ErrorDataReceived += (object sender, DataReceivedEventArgs e) =>
+                Console.WriteLine("error>>" + e.Data);
+            process.BeginErrorReadLine();
+
+            process.WaitForExit();
+
+            Console.WriteLine("ExitCode: {0}", process.ExitCode);
+            process.Close();
+        }
+
+        static void Main()
+        {
+            Process[] processes = Process.GetProcessesByName("blockland");
+
+            if(processes.Length > 0)
+            {
+                foreach(Process process in processes)
+                {
+                    Console.WriteLine($"Injecting into '{process.Id}' ...");
+                    // RemoteDLLInjector32.exe must be in Path or in same directory as this executable.
+                    ExecuteCommand($"RemoteDLLInjector32.exe {process.Id} Chams.dll");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Failed to find any instances of Blockland!");
+            }
+        }
+    }
+}
